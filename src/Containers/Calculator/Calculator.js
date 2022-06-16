7b31077f86d6db2cedf54e51dd3156545520f27c
infomagnum159 <infomagnum159@gmail.com>
2022-06-16 17:44:43 +0600

    Implemented the basic logic of the calculator

---
+++ src/Containers/Calculator/Calculator.js
@@ -0,0 +1,288 @@
+import React, {useState} from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+    const [mode, setMode] = useState('split');
+
+    const [people, setPeople] = useState([]);
+
+    const [split, setSplit] = useState({
+        peoples: 0,
+        sum: 0,
+        per: 15,
+        delivery: 0,
+    });
+
+    const [individual, setIndividual] = useState({
+        per: 15,
+        delivery: 0,
+        sum: 0,
+    });
+
+    const [totalSplit, setTotalSplit] = useState({
+        totalPrice: 0,
+        quantity: 0,
+        each: 0,
+    });
+
+    const [totalIndividual,setTotalIndividual] = useState([]);
+
+    const [showCheck, setShowCheck] = useState(false);
+
+    const onRadioChange = e => {
+        setMode(e.target.value);
+    };
+
+    const addPerson = () => {
+        setPeople(people => [...people, {name: '', price: '', id: Math.random()}])
+    };
+
+    const changePersonFields = (id, name, value) => {
+        setPeople(people => {
+            return people.map(person => {
+                if (person.id === id) {
+                    return {...person, [name]: value}
+                }
+
+                return person;
+            })
+        })
+    };
+
+    const removePersonFields = id => (
+        setPeople(people.filter(p => p.id !== id))
+    );
+
+    const onChangeInputs = e => {
+        const {name, value} = e.target;
+        setSplit(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    };
+
+    let calcSplit = null;
+    let calcIndividual = null;
+
+    if (showCheck) {
+        calcSplit = (
+            <div>
+                <div>Общая сумма: {totalSplit.totalPrice} сом</div>
+                <div>Кол-во человек: {totalSplit.quantity}</div>
+                <div>Каждый оплачивает: {totalSplit.each} сом</div>
+            </div>
+        );
+    }
+
+    if (showCheck) {
+        calcIndividual = (
+            <p>
+                <div>Общая сумма: {individual.sum} сом</div>
+                {totalIndividual.map(ind => (
+                    <div key={ind.id}>{ind.name}: {ind.totalPrice} сом</div>
+                ))}
+            </p>
+        )
+
+    }
+
+    const calculationSplit = e => {
+        e.preventDefault();
+        let delivery = 0;
+        if (split.delivery === undefined) {
+            delivery = 0
+        } else {
+            delivery = split.delivery
+        }
+        setTotalSplit(prev => ({
+            ...prev,
+            totalPrice: (split.sum * (split.per / 100)) + split.sum,
+            quantity: split.peoples,
+            each: Math.ceil((((split.sum * (split.per / 100)) + split.sum) / split.peoples) + (delivery / split.peoples)),
+        }));
+        setShowCheck(true);
+    };
+
+    const calculationIndividual = e => {
+        e.preventDefault();
+        let delivery = 0;
+        let total = 0;
+        if (individual.delivery === undefined) {
+            delivery = 0;
+        } else {
+            delivery = individual.delivery / people.length
+        }
+
+        setTotalIndividual(people.map(p => {
+            total += p.price;
+            return {
+                ...p,
+                name: p.name,
+                totalPrice: Math.ceil(p.price + (p.price * (individual.per / 100)) + delivery),
+                id: Math.random(),
+            }
+        }))
+
+        setShowCheck(true);
+        setIndividual(prev => ({
+            ...prev,
+            sum: total,
+        }));
+    };
+
+    const onChangeInputsInd = e => {
+        const {name, value} = e.target;
+        setIndividual(prev => ({
+            ...prev,
+            [name]: parseInt(value)
+        }))
+    };
+
+    return (
+        <div>
+            <p>
+                <label>
+                    <input
+                        type="radio"
+                        checked={mode === 'split'}
+                        name="mode"
+                        value="split"
+                        onChange={onRadioChange}
+                    />{' '}
+                    Поровну между участниками
+                </label>
+            </p>
+            <p>
+                <label>
+                    <input
+                        type="radio"
+                        checked={mode === 'individual'}
+                        name="mode"
+                        value="individual"
+                        onChange={onRadioChange}
+                    /> {' '}
+                    Каждому индивидуально
+                </label>
+            </p>
+
+            <div>
+                {mode === 'split' ? (
+                    <form className="Form" onSubmit={calculationSplit}>
+                        <label className="Split">
+                            Человек{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="peoples"
+                                value={split.peoples}
+                                onChange={onChangeInputs}
+                            />
+                            чел.
+                        </label>
+                        <label className="Split">
+                            Сумма заказа{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="sum"
+                                value={split.sum}
+                                onChange={onChangeInputs}
+                            />
+                            сом
+                        </label>
+                        <label className="Split">
+                            Обслуживание{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="per"
+                                value={split.per}
+                                onChange={onChangeInputs}
+                            />
+                            %
+                        </label>
+                        <label className="Split">
+                            Доставка{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="delivery"
+                                value={split.delivery}
+                                onChange={onChangeInputs}
+                            />
+                            сом
+                        </label>
+                        <p>
+                            <button
+                                type="submit"
+                            >
+                                Расчитать
+                            </button>
+                        </p>
+                        {calcSplit}
+
+                    </form>
+                ) : (
+                    <form onSubmit={calculationIndividual}>
+                        <h4>Individual</h4>
+                        <p>
+                            <button type="button" onClick={addPerson}>Add person</button>
+                        </p>
+                        {people.map(person => (
+                            <div key={person.id}>
+                                <input
+                                    type="text"
+                                    placeholder="Name"
+                                    value={person.name}
+                                    onChange={e => changePersonFields(person.id, 'name', e.target.value)}
+                                />
+                                <input
+                                    type="number"
+                                    placeholder="Total"
+                                    value={person.price}
+                                    onChange={e => changePersonFields(person.id, 'price', parseInt(e.target.value))}
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() => removePersonFields(person.id)}
+                                >
+                                    Remove
+                                </button>
+                            </div>
+                        ))}
+                        <label className="Split">
+                            Обслуживание{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="per"
+                                value={individual.per}
+                                onChange={onChangeInputsInd}
+                            />
+                            %
+                        </label>
+                        <label className="Split">
+                            Доставка{' '}
+                            <input
+                                className="SplitInput"
+                                type="number"
+                                name="delivery"
+                                value={individual.delivery}
+                                onChange={onChangeInputsInd}
+                            />
+                            сом
+                        </label>
+                        <button
+                            type="submit"
+                        >
+                            Рассчитать
+                        </button>
+                        {calcIndividual}
+                    </form>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Calculator;
